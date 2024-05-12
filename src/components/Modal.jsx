35a3ece5eb1efecb6ea6afbff132248f02c9c5ef
senaahmad2998 <senaahmad2998@gmail.com>
2024-05-12 13:30:08 +0700

    init: first init and commit source code

---
+++ src/components/Modal.jsx
@@ -0,0 +1,25 @@
+import { createPortal } from "react-dom";
+import { motion } from "framer-motion";
+
+export default function Modal({ title, children, onClose }) {
+  return createPortal(
+    <>
+      <div className="backdrop" onClick={onClose} />
+      <motion.dialog
+        variants={{
+          hidden: { y: 50, opacity: 0 },
+          visible: { y: 0, opacity: 1 },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        open
+        className="modal"
+      >
+        <h2>{title}</h2>
+        {children}
+      </motion.dialog>
+    </>,
+    document.getElementById("modal")
+  );
+}
